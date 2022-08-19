f3beb47a8f43762d138ec0bcec155555834b517e
ronnievdaniel <ronnievdaniel@live.com>
2022-08-19 12:06:27 +1000

    xxxxxxx

--- Week03-05/operate.py
+++ Week03-05/operate.py
@@ -3,17 +3,17 @@
 
 # basic python packages
 import numpy as np
-import cv2 
+import cv2
 import os, sys
 import time
 
 # import utility functions
 sys.path.insert(0, "{}/utility".format(os.getcwd()))
-from util.pibot import PenguinPi # access the robot
-import util.DatasetHandler as dh # save/load functions
-import util.measure as measure # measurements
-import pygame # python package for GUI
-import shutil # python package for file operations
+from util.pibot import PenguinPi  # access the robot
+import util.DatasetHandler as dh  # save/load functions
+import util.measure as measure  # measurements
+import pygame  # python package for GUI
+import shutil  # python package for file operations
 
 # import SLAM components
 sys.path.insert(0, "{}/slam".format(os.getcwd()))
@@ -30,7 +30,7 @@ class Operate:
         else:
             shutil.rmtree(self.folder)
             os.makedirs(self.folder)
-        
+
         # initialise data parameters
         if args.play_data:
             self.pibot = dh.DatasetPlayer("record")
@@ -40,14 +40,14 @@ class Operate:
         # initialise SLAM parameters
         self.ekf = self.init_ekf(args.calib_dir, args.ip)
         self.aruco_det = aruco.aruco_detector(
-            self.ekf.robot, marker_length = 0.07) # size of the ARUCO markers
+            self.ekf.robot, marker_length=0.07)  # size of the ARUCO markers
 
         if args.save_data:
             self.data = dh.DatasetWriter('record')
         else:
             self.data = None
         self.output = dh.OutputWriter('lab_output')
-        self.command = {'motion':[0, 0], 
+        self.command = {'motion': [0, 0],
                         'inference': False,
                         'output': False,
                         'save_inference': False,
@@ -65,14 +65,14 @@ class Operate:
         self.start_time = time.time()
         self.control_clock = time.time()
         # initialise images
-        self.img = np.zeros([240,320,3], dtype=np.uint8)
-        self.aruco_img = np.zeros([240,320,3], dtype=np.uint8)
+        self.img = np.zeros([240, 320, 3], dtype=np.uint8)
+        self.aruco_img = np.zeros([240, 320, 3], dtype=np.uint8)
         self.bg = pygame.image.load('pics/gui_mask.jpg')
 
     # wheel control
-    def control(self):       
+    def control(self):
         if args.play_data:
-            lv, rv = self.pibot.set_velocity()            
+            lv, rv = self.pibot.set_velocity()
         else:
             lv, rv = self.pibot.set_velocity(
                 self.command['motion'])
@@ -82,13 +82,14 @@ class Operate:
         drive_meas = measure.Drive(lv, rv, dt)
         self.control_clock = time.time()
         return drive_meas
+
     # camera control
     def take_pic(self):
         self.img = self.pibot.get_image()
         if not self.data is None:
             self.data.write_image(self.img)
 
-    # SLAM with ARUCO markers       
+    # SLAM with ARUCO markers
     def update_slam(self, drive_meas):
         lms, self.aruco_img = self.aruco_det.detect_marker_positions(self.img)
         if self.request_recover_robot:
@@ -100,7 +101,7 @@ class Operate:
                 self.notification = 'Recover failed, need >2 landmarks!'
                 self.ekf_on = False
             self.request_recover_robot = False
-        elif self.ekf_on: # and not self.debug_flag:
+        elif self.ekf_on:  # and not self.debug_flag:
             self.ekf.predict(drive_meas)
             self.ekf.add_landmarks(lms)
             self.ekf.update(lms)
@@ -126,7 +127,7 @@ class Operate:
         scale = np.loadtxt(fileS, delimiter=',')
         if ip == 'localhost':
             scale /= 2
-        fileB = "{}baseline.txt".format(datadir)  
+        fileB = "{}baseline.txt".format(datadir)
         baseline = np.loadtxt(fileB, delimiter=',')
         robot = Robot(baseline, scale, camera_matrix, dist_coeffs)
         return EKF(robot)
@@ -138,7 +139,7 @@ class Operate:
             self.notification = 'Map is saved'
             self.command['output'] = False
 
-    # paint the GUI            
+    # paint the GUI
     def draw(self, canvas):
         canvas.blit(self.bg, (0, 0))
         text_colour = (220, 220, 220)
@@ -146,33 +147,33 @@ class Operate:
         h_pad = 20
 
         # paint SLAM outputs
-        ekf_view = self.ekf.draw_slam_state(res=(320, 480+v_pad),
-            not_pause = self.ekf_on)
-        canvas.blit(ekf_view, (2*h_pad+320, v_pad))
+        ekf_view = self.ekf.draw_slam_state(res=(320, 480 + v_pad),
+                                            not_pause=self.ekf_on)
+        canvas.blit(ekf_view, (2 * h_pad + 320, v_pad))
         robot_view = cv2.resize(self.aruco_img, (320, 240))
-        self.draw_pygame_window(canvas, robot_view, 
+        self.draw_pygame_window(canvas, robot_view,
                                 position=(h_pad, v_pad)
                                 )
 
         # canvas.blit(self.gui_mask, (0, 0))
-        self.put_caption(canvas, caption='SLAM', position=(2*h_pad+320, v_pad)) # M2
+        self.put_caption(canvas, caption='SLAM', position=(2 * h_pad + 320, v_pad))  # M2
         self.put_caption(canvas, caption='Detector (M3)',
-                         position=(h_pad, 240+2*v_pad)) # M3
+                         position=(h_pad, 240 + 2 * v_pad))  # M3
         self.put_caption(canvas, caption='PiBot Cam', position=(h_pad, v_pad))
 
         notifiation = TEXT_FONT.render(self.notification,
-                                          False, text_colour)
-        canvas.blit(notifiation, (h_pad+10, 596))
+                                       False, text_colour)
+        canvas.blit(notifiation, (h_pad + 10, 596))
 
         time_remain = self.count_down - time.time() + self.start_time
         if time_remain > 0:
             time_remain = f'Count Down: {time_remain:03.0f}s'
-        elif int(time_remain)%2 == 0:
+        elif int(time_remain) % 2 == 0:
             time_remain = "Time Is Up !!!"
         else:
             time_remain = ""
         count_down_surface = TEXT_FONT.render(time_remain, False, (50, 50, 50))
-        canvas.blit(count_down_surface, (2*h_pad+320+5, 530))
+        canvas.blit(count_down_surface, (2 * h_pad + 320 + 5, 530))
         return canvas
 
     @staticmethod
@@ -181,18 +182,17 @@ class Operate:
         view = pygame.surfarray.make_surface(cv2_img)
         view = pygame.transform.flip(view, True, False)
         canvas.blit(view, position)
-    
+
     @staticmethod
     def put_caption(canvas, caption, position, text_colour=(200, 200, 200)):
         caption_surface = TITLE_FONT.render(caption,
-                                          False, text_colour)
-        canvas.blit(caption_surface, (position[0], position[1]-25))
+                                            False, text_colour)
+        canvas.blit(caption_surface, (position[0], position[1] - 25))
 
-    # keyboard teleoperation        
+    # keyboard teleoperation
     def update_keyboard(self):
         for event in pygame.event.get():
             ########### replace with your M1 codes ###########
-            # drive forward
             if event.type == pygame.KEYDOWN and event.key == pygame.K_q:
                 self.command['motion'] = [1, 1]
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_w:
@@ -227,7 +227,7 @@ class Operate:
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
                 if self.double_reset_comfirm == 0:
                     self.notification = 'Press again to confirm CLEAR MAP'
-                    self.double_reset_comfirm +=1
+                    self.double_reset_comfirm += 1
                 elif self.double_reset_comfirm == 1:
                     self.notification = 'SLAM Map is cleared'
                     self.double_reset_comfirm = 0
@@ -260,7 +260,7 @@ class Operate:
             pygame.quit()
             sys.exit()
 
-        
+
 if __name__ == "__main__":
     import argparse
 
@@ -271,11 +271,11 @@ if __name__ == "__main__":
     parser.add_argument("--save_data", action='store_true')
     parser.add_argument("--play_data", action='store_true')
     args, _ = parser.parse_known_args()
-    
-    pygame.font.init() 
+
+    pygame.font.init()
     TITLE_FONT = pygame.font.Font('pics/8-BitMadness.ttf', 35)
     TEXT_FONT = pygame.font.Font('pics/8-BitMadness.ttf', 40)
-    
+
     width, height = 700, 660
     canvas = pygame.display.set_mode((width, height))
     pygame.display.set_caption('ECE4078 2021 Lab')
@@ -285,7 +285,7 @@ if __name__ == "__main__":
     pibot_animate = [pygame.image.load('pics/8bit/pibot1.png'),
                      pygame.image.load('pics/8bit/pibot2.png'),
                      pygame.image.load('pics/8bit/pibot3.png'),
-                    pygame.image.load('pics/8bit/pibot4.png'),
+                     pygame.image.load('pics/8bit/pibot4.png'),
                      pygame.image.load('pics/8bit/pibot5.png')]
     pygame.display.update()
 
@@ -299,23 +299,22 @@ if __name__ == "__main__":
         canvas.blit(splash, (0, 0))
         x_ = min(counter, 600)
         if x_ < 600:
-            canvas.blit(pibot_animate[counter%10//2], (x_, 565))
+            canvas.blit(pibot_animate[counter % 10 // 2], (x_, 565))
             pygame.display.update()
             counter += 2
 
     operate = Operate(args)
-    t_0 = time.time()
-    loop_dt = 0.1
+
     while start:
-        if time.time() - t_0 > loop_dt:
-            operate.update_keyboard()
-            operate.take_pic()
-            drive_meas = operate.control()
-            operate.save_image()
-            # visualise
-            operate.draw(canvas)
-            pygame.display.update()
-            t_0 = time.time()
+        operate.update_keyboard()
+        operate.take_pic()
+        drive_meas = operate.control()
+        operate.update_slam(drive_meas)
+        operate.record_data()
+        operate.save_image()
+        # visualise
+        operate.draw(canvas)
+        pygame.display.update()
 
 
 
